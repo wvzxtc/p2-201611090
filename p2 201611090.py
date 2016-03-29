6cfb6d180d363670a07fb248e22f6953096f83af
wvzxtc <wvzxtc@daum.net>
2016-03-30 00:06:51 +0900

    123456

---
+++ p2 201611090.py
@@ -0,0 +1,14 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+def makeSquare(size,bigger,sides,angle):
+     for i in range(0,sides):
+          if(i%2):
+              size=size+bigger
+          t1.fd(size)
+          t1.right(90)
+
+makelSquare(20,20,30,90)
+
+wn.exitonclick()
