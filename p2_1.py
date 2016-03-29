36dd92985fcda12614e29b69232dcbc3b6edbd4b
wvzxtc <wvzxtc@daum.net>
2016-03-30 00:58:32 +0900

    123456

---
+++ p2_1.py
@@ -0,0 +1,15 @@
+﻿%%plantuml
+@startuml
+start
+:set how many turns;
+:set how much to grow bigger;
+:set angle;
+:set start size;
+repeat
+if (i is divided by 2) then
+    :grow bigger;
+endif
+:draw;
+repeat while(turns)
+stop
+@enduml
