eeed873e5a0aaac3490c4cf88b35106677cc151d
wvzxtc <wvzxtc@daum.net>
2016-04-05 08:14:24 +0900

    123456

---
+++ w5Main8.py
@@ -0,0 +1,16 @@
+﻿height = input("Put in your height : ")
+weight = input("Put in your weight : ")
+BMI = weight * 10000 / (height * height)
+print BMI
+if BMI <= 18.5 :
+    print "Underweight"
+elif BMI > 18.5 and BMI <= 23 :
+    print "Normalweight"
+elif BMI > 23 and BMI <= 25 :
+    print "Overweight"
+elif BMI > 25 and BMI <= 30 :
+    print "Obesity"
+elif BMI > 30 :
+    print "An extremely obese"
+else :
+    print "Input error"
