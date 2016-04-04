12e83b22bba2529248fad3e82c3aead63f37e375
wvzxtc <wvzxtc@daum.net>
2016-04-04 15:37:05 +0900

    Added files via upload

---
+++ w5Main7.py
@@ -0,0 +1,23 @@
+﻿Python 2.7.11 |Anaconda 2.5.0 (32-bit)| (default, Jan 29 2016, 15:36:56) [MSC v.
+1500 32 bit (Intel)]
+Type "copyright", "credits" or "license" for more information.
+
+IPython 4.0.3 -- An enhanced Interactive Python.
+?         -> Introduction and overview of IPython's features.
+%quickref -> Quick reference.
+help      -> Python's own help system.
+object?   -> Details about 'object', use 'object??' for extra details.
+
+In [1]: for i in range(7):
+   ...:     print " "*(7-i)+"#"*i+"#"*(i-1)
+   ...:
+
+         #
+        ###
+       #####
+      #######
+     #########
+    ###########
+   #############
+  ###############
+ #################
