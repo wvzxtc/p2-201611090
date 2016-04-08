3e777105162a752090e75954e7762e9718331341
wvzxtc <wvzxtc@daum.net>
2016-04-08 11:06:38 +0900

    Update w6Main11.py

--- w6Main11.py
+++ w6Main11.py
@@ -1,5 +1,5 @@
 ﻿year=input("User input year : ")
-if (year%4==0) and (year%100!=0 or year%400==0) :
+if (year%4==0 and year%100!=0) and (year%400==0) :
     print "Leap year"
 else:
     print "Not Leap year"
