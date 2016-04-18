ede304c71aa882483f1e1540282a9d8db7ede85e
wvzxtc <wvzxtc@daum.net>
2016-04-18 15:55:11 +0900

    Added files via upload

---
+++ w7main.py
@@ -0,0 +1,19 @@
+﻿def drawSquareAt(size, pos):
+    t1.penup()
+    t1.setpos(pos)
+    t1.pendown()
+    tracks=list()
+    for i in range(0,4):
+        tracks.append(t1,pos())
+        t1.forward(size)
+        t1.right(90)
+    return tracks
+
+def lab7a():
+    size=100
+    pos=t1.pos
+    mytrack=drawSquareAt(size, pos)
+    print mytrack
+    
+def main():
+    lab7a()
