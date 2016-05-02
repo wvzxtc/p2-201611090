7fa2bbcb7f61bc8c4dd7db370eca2cbbe7523894
wvzxtc <wvzxtc@daum.net>
2016-05-02 15:44:15 +0900

    Added files via upload

---
+++ w9main.py
@@ -0,0 +1,20 @@
+﻿def CharCount():
+    sentence="sangmyung university"
+    allchars= list(sentence)
+    
+    d=dict()
+    for c in allchars:
+        if c not in d:
+            d[c]=1
+        else:
+            d[c]=d[c]+1
+
+            
+    import matplotlib
+    import matplotlib.pyplot as plt
+    
+    plt.bar(range(len(d)),d.values(), align='center')
+    plt.bar(range(len(d)), list(d.keys()))
+    plt.show()
+
+Charcount()
