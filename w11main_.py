e55f4025457d95d6bfd00f2a9d2837bbe1f93882
wvzxtc <wvzxtc@daum.net>
2016-05-13 17:57:49 +0900

    Add files via upload

---
+++ w11main_.py
@@ -0,0 +1,70 @@
+﻿chae=[ "Our Constitution declares that from time to time, the President shall give to Congress information about the state of our Union. For 220 years, our leaders have fulfilled this duty. They've done so during periods of prosperity and tranquility, and they've done so in the midst of war and depression, at moments of great strife and great struggle.
+
+It's tempting to look back on these moments and assume that our progress was inevitable, that America was always destined to succeed. But when the Union was turned back at Bull Run and the Allies first landed at Omaha Beach, victory was very much in doubt. When the market crashed on Black Tuesday and civil rights marchers were beaten on Bloody Sunday, the future was anything but certain. These were the times that tested the courage of our convictions and the strength of our Union. And despite all our divisions and disagreements, our hesitations and our fears, America prevailed because we chose to move forward as one Nation, as one people. Again, we are tested. And again, we must answer history's call.
+
+One year ago, I took office amid two wars, an economy rocked by a severe recession, a financial system on the verge of collapse, and a Government deeply in debt. Experts from across the political spectrum warned that if we did not act, we might face a second depression. So we acted, immediately and aggressively. And 1 year later, the worst of the storm has passed.
+
+But the devastation remains. One in 10 Americans still cannot find work. Many businesses have shuttered. Home values have declined. Small towns and rural communities have been hit especially hard. And for those who'd already known poverty, life's become that much harder.
+
+This recession has also compounded the burdens that America's families have been dealing with for decades: the burden of working harder and longer for less, of being unable to save enough to retire or help kids with college.
+
+So I know the anxieties that are out there right now. They're not new. These struggles are the reason I ran for President. These struggles are what I've witnessed for years, in places like Elkhart, Indiana; Galesburg, Illinois. I hear about them in the letters that I read each night. The toughest to read are those written by children asking why they have to move from their home, asking when their mom or dad will be able to go back to work.
+
+For these Americans and so many others, change has not come fast enough. Some are frustrated, some are angry. They don't understand why it seems like bad behavior on Wall Street is rewarded, but hard work on Main Street isn't, or why Washington has been unable or unwilling to solve any of our problems. They're tired of the partisanship and the shouting and the pettiness. They know we can't afford it. Not now.
+
+So we face big and difficult challenges. And what the American people hope, what they deserve, is for all of us, Democrats and Republicans, to work through our differences, to overcome the numbing weight of our politics. For while the people who sent us here have different backgrounds, different stories, different beliefs, the anxieties they face are the same. The aspirations they hold are shared: a job that pays the bills; a chance to get ahead; most of all, the ability to give their children a better life.
+
+And you know what else they share? They share a stubborn resilience in the face of adversity. After one of the most difficult years in our history, they remain busy building cars and teaching kids, starting businesses and going back to school. They're coaching Little League and helping their neighbors. One woman wrote to me and said, "We are strained but hopeful, struggling but encouraged."
+
+It's because of this spirit, this great decency and great strength, that I have never been more hopeful about America's future than I am tonight. Despite our hardships, our Union is strong. We do not give up. We do not quit. We do not allow fear or division to break our spirit. In this new decade, it's time the American people get a Government that matches their decency, that embodies their strength. And tonight I'd like to talk about how together we can deliver on that promise.
+
+It begins with our economy. Our most urgent task upon taking office was to shore up the same banks that helped cause this crisis. It was not easy to do. And if there's one thing that has unified Democrats and Republicans and everybody in between, it's that we all hated the bank bailout. I hated it. I hated it; you hated it. It was about as popular as a root canal. 
+
+But when I ran for President, I promised I wouldn't just do what was popular; I would do what was necessary. And if we had allowed the meltdown of the financial system, unemployment might be double what it is today. More businesses would certainly have closed. More homes would have surely been lost.
+
+So I supported the last administration's efforts to create the financial rescue program. And when we took that program over, we made it more transparent and more accountable. And as a result, the markets are now stabilized, and we've recovered most of the money we spent on the banks—most but not all.
+
+To recover the rest, I've proposed a fee on the biggest banks. Now, I know Wall Street isn't keen on this idea. But if these firms can afford to hand out big bonuses again, they can afford a modest fee to pay back the taxpayers who rescued them in their time of need.
+
+Now, as we stabilized the financial system, we also took steps to get our economy growing again, save as many jobs as possible, and help Americans who had become unemployed. That's why we extended or increased unemployment benefits for more than 18 million Americans, made health insurance 65 percent cheaper for families who get their coverage through COBRA, and passed 25 different tax cuts.
+
+Now, let me repeat: We cut taxes. We cut taxes for 95 percent of working families. We cut taxes for small businesses. We cut taxes for first-time home buyers. We cut taxes for parents trying to care for their children. We cut taxes for 8 million Americans paying for college. I thought I'd get some applause on that one. 
+
+As a result, millions of Americans had more to spend on gas and food and other necessities, all of which helped businesses keep more workers. And we haven't raised income taxes by a single dime on a single person—not a single dime.
+
+Now, because of the steps we took, there are about 2 million Americans working right now who would otherwise be unemployed. Two hundred thousand work in construction and clean energy. Three hundred thousand are teachers and other education workers. Tens of thousands are cops, firefighters, correctional officers, first-responders. And we're on track to add another 1½ million jobs to this total by the end of the year.
+
+The plan that has made all of this possible, from the tax cuts to the jobs, is the Recovery Act. That's right, the Recovery Act, also known as the stimulus bill. Economists on the left and the right say this bill has helped save jobs and avert disaster. But you don't have to take their word for it. Talk to the small business in Phoenix that will triple its workforce because of the Recovery Act. Talk to the window manufacturer in Philadelphia who said he used to be skeptical about the Recovery Act, until he had to add two more work shifts just because of the business it created. Talk to the single teacher raising two kids who was told by her principal in the last week of school that because of the Recovery Act, she wouldn't be laid off after all.
+
+There are stories like this all across America. And after 2 years of recession, the economy is growing again. Retirement funds have started to gain back some of their value. Businesses are beginning to invest again, and slowly some are starting to hire again.
+
+But I realize that for every success story, there are other stories, of men and women who wake up with the anguish of not knowing where their next paycheck will come from, who send out resumes week after week and hear nothing in response. That is why jobs must be our number-one focus in 2010, and that's why I'm calling for a new jobs bill tonight.
+
+Now, the true engine of job creation in this country will always be America's businesses.", " I agree, absolutely.", " But Government can create the conditions necessary for businesses to expand and hire more workers.", " We should start where most new jobs do, in small businesses, companies that begin when an entrepreneur takes a chance on a dream or a worker decides it's time she became her own boss.", " Through sheer grit and determination, these companies have weathered the recession, and they're ready to grow.", " But when you talk to small-business owners in places like Allentown, Pennsylvania, or Elyria, Ohio, you find out that even though banks on Wall Street are lending again, they're mostly lending to bigger companies.", " Financing remains difficult for small-business owners across the country, even those that are making a profit.", "
+
+So tonight I'm proposing that we take $30 billion of the money Wall Street banks have repaid and use it to help community banks give small businesses the credit they need to stay afloat.", " I'm also proposing a new small business tax credit, one that will go to over 1 million small businesses who hire new workers or raise wages." ,"While we're at it, let's also eliminate all capital gains taxes on small-business investment and provide a tax incentive for all large businesses and all small businesses to invest in new plants and equipment."]
+word1=dict()
+word2=dict()
+num1=[0,0,0,0,0,0,0,0,0,0]
+wordlist1=["","","","","","","","","",""]
+num2=[0,0,0,0,0,0,0,0,0,0]
+wordlist2=["","","","","","","","","",""]
+def countwords(chae,num1,word1,wordlist1):
+    for i in chae:
+        words=i.lower()
+        words=words.split()
+        for l in words:
+            if l not in word1:
+                word1[l]=1
+            else:
+                word1[l]=word1[l]+1
+    for i in word1:
+        for number in range(0,10):
+            if word1[i]>num1[number]:
+                num1[number]=word1[i]
+                wordlist1[number]=i
+                break
+    print num1
+    print wordlist1
+countwords(chae,num1,word1,wordlist1)
+countwords(add2,num2,word2,wordlist2)
